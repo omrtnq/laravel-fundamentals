7492ca7012f424208861366e2090df1ca76b33e7
William Corotan <williamcorotan@gmail.com>
2023-01-30 22:16:28 +0800

    Modified student view

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,35 +1,35 @@
-<!DOCTYPE html>
-<html lang="en" class="dark">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Students</title>
-  @vite('resources/css/app.css')
-</head>
-<body class="p-4">
-  <ul class="text-center grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-y-4">
+@include('partials.header')
 
-    @foreach ($data as $item)
-    <li class="py-4 bg-gray-600 border-8  border-gray-600 rounded-lg w-4/5 mx-auto">
-      <ul class="text-white">        
-        <li>{{ $item->first_name }} {{ $item->last_name }} </li>
-        <li>Age: {{ $item->age }}</li>
-        <li >Gender: {{ $item->gender }}</li>   
-      </ul>
-    </li>
-    @endforeach
-    {{-- @foreach ($data as $item)
+<x-navbar />
 
-    <li class="py-4 border-8 border-green-400 w-3/5 my-4 mx-auto">
-      <ul>
-        <li>{{ $item->gender }}: {{ $item->gender_count }}</li>
-     
-      </ul>
-    </li>
-    @endforeach     --}}
-  </ul>
-  
-</body>
-</html>
+<header class="max-w-lg mx-auto my-8">
+  <a href="#">
+    <h1 class="text-4xl font-bold  text-center">Student List</h1>
+  </a>
+</header>
 
+<section class="grid grid-cols-1 lg:grid-cols-2  gap-4 lg:gap-8 w-4/5 mx-auto">
+  @foreach ($data as $item)
+  <div class="justify-center items-center">
+    <div class="flex px-4 rounded-lg shadow-lg gap-2 bg-white min-w-sm">
+      {{-- DIV FOR PROFILE IMAGE --}}
+      <div class="m-4"> 
+        @if ($item->gender === 'Male')
+        <img src="https://cdn-icons-png.flaticon.com/512/201/201818.png" class="w-32 rounded-full" alt="">
+        <img src="picture.svg" alt="">     
+        @else  
+        <img src="https://cdn-icons-png.flaticon.com/512/6997/6997662.png" class="w-32 rounded-full" alt="">
+        @endif
+      </div>
+      {{-- DIV FOR STUDENT INFO --}}
+      <div class="my-auto">
+        <h5 class="text-gray-900 text-2xl leading-tight font-medium ">{{ $item->first_name }} {{ $item->last_name }}</h5>
+        <p class="text-gray-700 text-sm">{{ $item->email }}</p>
+        <p class="text-gray-700 text-xs mt-2">{{ $item->gender }} | {{ $item->age }} years old </p>
+      </div>
+    </div>
+  </div>
+  @endforeach
+</section>
+
+@include('partials.footer')
